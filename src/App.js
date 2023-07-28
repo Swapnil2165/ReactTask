ac244f1ae9ac022241cd9bde0609f5c2ef044c90
swapnil2165 <swapnilchikanepatil2165@gmail.com>
2023-07-28 23:05:05 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 
 
 import Task1 from "./Task1/Navbar";
-import Task2A from "./Task2A/Form"
-import Task2B from "./Task2B/Form"
-import Task3 from "./Task3/Seaction"
-import Task4 from "./Task4/Box"
-import Task5 from "./Task5/Staff"
 
 
 
@@ -14,12 +9,8 @@ import Task5 from "./Task5/Staff"
 function App() {
   return (
     <div>
-     {/* <Task1/>
-      <Task2B/>
-     <Task3/>
-     <Task4/>
-     <Task5/> */}
-     <Task2A/>
+     <Task1/>
+     
     </div>
   );
 }
